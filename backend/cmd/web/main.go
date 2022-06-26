c2f0f1ce0396545cb1161caa9acd2b87b65fc376
Nathanael Cunningham <nathanaelcunningham@gmail.com>
2022-06-26 21:50:46 +0000

    add env to backend

--- backend/cmd/web/main.go
+++ backend/cmd/web/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"flag"
 	"log"
 	"nathanaelcunningham/voe-quotes/pkg/models/mysql"
 	"net/http"
@@ -15,21 +14,19 @@ import (
 type application struct {
 	infoLog  *log.Logger
 	errorLog *log.Logger
-	quotes *mysql.QuoteModel
+	quotes   *mysql.QuoteModel
 }
 
 func main() {
-	//parse flags
-	//address for starting server
-	addr := flag.String("addr", ":3000", "HTTP network address")
-	//dsn for database connection
-	dsn := flag.String("dsn", "quotes:quotes@tcp(mariadb:3306)/quotes?parseTime=true", "MariaDB DSN")
-	flag.Parse()
+	//Get Env variables
+	dsn := os.Getenv("DSN")
+	port := os.Getenv("PORT")
 
+	//Setup loggers
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	db, err := openDB(*dsn)
+	db, err := openDB(dsn)
 	if err != nil {
 		errorLog.Fatal(err)
 	}
@@ -39,18 +36,18 @@ func main() {
 	app := &application{
 		infoLog:  infoLog,
 		errorLog: errorLog,
-		quotes: &mysql.QuoteModel{DB: db},
+		quotes:   &mysql.QuoteModel{DB: db},
 	}
 
 	srv := &http.Server{
-		Addr:         *addr,
+		Addr:         port,
 		Handler:      app.routes(),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
 
-	infoLog.Printf("Starting server on %s", *addr)
+	infoLog.Printf("Starting server on %s", port)
 	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
